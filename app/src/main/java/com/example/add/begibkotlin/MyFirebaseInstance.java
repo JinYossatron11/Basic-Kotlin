b1de630999c9fce23ec3cf782e53be99246e8640
Todsphol <theaddza11@gmail.com>
2017-08-16 16:39:41 +0700

    Create class MyFirebaseInstance and Activity

---
+++ app/src/main/java/com/example/add/begibkotlin/MyFirebaseInstance.java
@@ -0,0 +1,13 @@
+package com.example.add.begibkotlin;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyFirebaseInstance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_firebase_instance);
+    }
+}
