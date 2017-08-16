3099ec0dd169b43af74cc0c3b8eb88f652ad91d4
Todsphol <theaddza11@gmail.com>
2017-08-16 17:31:13 +0700

    Convert java to Kotlin

---
+++ app/src/main/java/com/example/add/begibkotlin/MyFirebaseInstance.kt
@@ -0,0 +1,55 @@
+package com.example.add.begibkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.widget.Toast
+
+import com.google.firebase.iid.FirebaseInstanceId
+import com.google.firebase.messaging.FirebaseMessaging
+
+import com.google.firebase.messaging.FirebaseMessaging.*
+
+class MyFirebaseInstance : AppCompatActivity(), View.OnClickListener {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_my_firebase_instance)
+        if (intent.extras != null) {
+            for (key in intent.extras.keySet()) {
+                val value = intent.extras.get(key)
+                Log.d(TAG, "Key: $key Value: $value")
+            }
+        }
+
+        findViewById(R.id.subscribeButton).setOnClickListener(this)
+        findViewById(R.id.unsubscribeButton).setOnClickListener(this)
+        findViewById(R.id.logTokenButton).setOnClickListener(this)
+    }
+
+    override fun onClick(view: View) {
+
+        when (view.id) {
+            R.id.subscribeButton -> {
+                getInstance().subscribeToTopic("news")
+                Log.d(TAG, "SubscribeToTopic")
+                Toast.makeText(this@MyFirebaseInstance, "SubscribeToTopic", Toast.LENGTH_SHORT).show()
+            }
+            R.id.unsubscribeButton -> {
+                getInstance().unsubscribeFromTopic("news")
+                Log.d(TAG, "UnsubscribeFromTopic")
+                Toast.makeText(this@MyFirebaseInstance, "UnsubscribeFromTopic", Toast.LENGTH_SHORT).show()
+            }
+            R.id.logTokenButton -> {
+                val token = FirebaseInstanceId.getInstance().token
+                Log.d(TAG, "Token : " + token!!)
+                Toast.makeText(this@MyFirebaseInstance, "Token : " + token, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    companion object {
+        private val TAG = "MainActivity"
+    }
+}
