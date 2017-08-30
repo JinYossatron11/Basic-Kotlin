669591244101f5e3a85bc17047814cba72948a12
Todsphol <theaddza11@gmail.com>
2017-08-30 23:47:54 +0700

    Add method setImageDrawable and notifyDrawable in class CustomImageView

--- app/src/main/java/com/example/add/begibkotlin/coursepeewee/view/CustomImageView.java
+++ app/src/main/java/com/example/add/begibkotlin/coursepeewee/view/CustomImageView.java
@@ -1,6 +1,7 @@
 package com.example.add.begibkotlin.coursepeewee.view;
 
 import android.content.Context;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
 
@@ -12,5 +13,16 @@ public class CustomImageView extends AppCompatImageView {
 
     public CustomImageView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void setImageDrawable(Drawable drawable) {
+        final Drawable previous = getDrawable();
+        super.setImageDrawable(drawable);
+
+    }
+
+    private static void notifyDrawable(Drawable drawable, final boolean isDisplayed) {
+
     }
 }
