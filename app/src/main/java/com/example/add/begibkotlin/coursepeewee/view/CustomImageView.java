ac81dedaee5c538945667ef971555a2f5230a23e
Todsphol <theaddza11@gmail.com>
2017-08-30 23:42:04 +0700

    Improve Custom Image View extends AppCompatImageView

--- app/src/main/java/com/example/add/begibkotlin/coursepeewee/view/CustomImageView.java
+++ app/src/main/java/com/example/add/begibkotlin/coursepeewee/view/CustomImageView.java
@@ -1,8 +1,16 @@
 package com.example.add.begibkotlin.coursepeewee.view;
 
-/**
- * Created by add on 30/8/2560.
- */
+import android.content.Context;
+import android.support.v7.widget.AppCompatImageView;
+import android.util.AttributeSet;
 
-public class CustomImageView {
+public class CustomImageView extends AppCompatImageView {
+
+    public CustomImageView(Context context) {super(context);}
+
+    public CustomImageView(Context context, AttributeSet attrs) {super(context, attrs);}
+
+    public CustomImageView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
 }
