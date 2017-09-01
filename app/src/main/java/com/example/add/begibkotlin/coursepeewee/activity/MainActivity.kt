c6aea1115b162802821acf1b4016dae32afe6ee8
Todsphol <theaddza11@gmail.com>
2017-09-01 20:44:21 +0700

    Change Edit Text and Botton in MainFragment to MainActivity

--- app/src/main/java/com/example/add/begibkotlin/coursepeewee/activity/MainActivity.kt
+++ app/src/main/java/com/example/add/begibkotlin/coursepeewee/activity/MainActivity.kt
@@ -5,32 +5,30 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.MenuItem
+import android.widget.Button
+import android.widget.CheckBox
+import android.widget.EditText
 import com.example.add.begibkotlin.R
 import com.example.add.begibkotlin.coursepeewee.fragment.MainFragment
+import com.example.add.begibkotlin.view.CustomCheckBox
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtUsername : EditText
+    lateinit var edtPassword : EditText
+    lateinit var cbRemember  : CustomCheckBox
+    lateinit var btLogin     : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
-                .commit()
     }
     fun changeFragment(fragment: Fragment) {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, fragment)
                 .addToBackStack(null)
                 .commit()
-    }
-
-    override
-    fun onOptionsItemSelected(item: MenuItem): Boolean {
-        when (item.itemId) {
-            android.R.id.home -> onBackPressed()
-        }
-        return false
     }
 
 }
