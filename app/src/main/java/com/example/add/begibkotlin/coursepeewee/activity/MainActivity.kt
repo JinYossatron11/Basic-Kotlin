0cd5750d474679a36c0f5ec295509eae3d4a5a7f
Todsphol <theaddza11@gmail.com>
2017-09-01 21:18:14 +0700

    Improve First Page and Add Check Box

--- app/src/main/java/com/example/add/begibkotlin/coursepeewee/activity/MainActivity.kt
+++ app/src/main/java/com/example/add/begibkotlin/coursepeewee/activity/MainActivity.kt
@@ -5,19 +5,10 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.MenuItem
-import android.widget.Button
-import android.widget.CheckBox
-import android.widget.EditText
 import com.example.add.begibkotlin.R
 import com.example.add.begibkotlin.coursepeewee.fragment.MainFragment
-import com.example.add.begibkotlin.view.CustomCheckBox
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var edtUsername : EditText
-    lateinit var edtPassword : EditText
-    lateinit var cbRemember  : CustomCheckBox
-    lateinit var btLogin     : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -28,6 +19,12 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.container, fragment)
                 .addToBackStack(null)
+                .commit()
+    }
+
+    fun startFragment() {
+        supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
                 .commit()
     }
 
